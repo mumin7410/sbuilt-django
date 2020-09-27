db42c17677ed50ea2039679260a7f91a8908babb
mumin7410 <mumin96303@gmail.com>
2020-09-28 06:49:56 +0700

    set debug false

--- webapp/settings.py
+++ webapp/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't_g!q0+my&_)z9id+ppgb9va!5#1nmprpx5(^)1_l(!$5#g@7u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['sbuit1.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
