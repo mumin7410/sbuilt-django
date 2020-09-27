3ec7be8fab6f74d4c1061159846dbd7cbd74666a
mumin7410 <mumin96303@gmail.com>
2020-09-27 21:08:39 +0700

    first commit

--- webapp/settings.py
+++ webapp/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 ]
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 ROOT_URLCONF = 'webapp.urls'
