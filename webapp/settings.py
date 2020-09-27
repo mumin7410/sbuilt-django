ba6280273449c8348e11c7cc3063de6dff203bbc
mumin7410 <mumin96303@gmail.com>
2020-09-27 20:56:40 +0700

    first commit

--- webapp/settings.py
+++ webapp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 't_g!q0+my&_)z9id+ppgb9va!5#1nmprpx5(^)1_l(!$5#g@7u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sbuilt.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['sbuilt.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
