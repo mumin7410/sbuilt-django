1a7f3c0098f8737c2ea5ab254235545170d76088
mumin7410 <mumin96303@gmail.com>
2020-09-28 07:25:30 +0700

    set debug true

--- webapp/settings.py
+++ webapp/settings.py
@@ -28,11 +28,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't_g!q0+my&_)z9id+ppgb9va!5#1nmprpx5(^)1_l(!$5#g@7u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sbuit1.herokuapp.com','127.0.0.1']
 
-LOGGING = { 'version': 1, 'disable_existing_loggers': False, 'handlers': { 'console': { 'class': 'logging.StreamHandler', }, }, 'loggers': 
+
 # Application definition
 
 
